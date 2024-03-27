5c384b780a6f468e289d50a217d8b8f2ed9c215e
FrenzyVJN <svijayan01@gmail.com>
2024-03-27 15:48:27 +0530

    calculator working

--- src/routes/calculator/+page.svelte
+++ src/routes/calculator/+page.svelte
@@ -2,71 +2,126 @@
     import { onMount } from 'svelte';
     import data from '../../data.json';
 
-    let branch: string = '';
-    let year: number = 0;
-    let subject1: string = '';
-    let subject2: string = '';
+    let branch: string;
+    let sem: number;
+    let grade: string[] = Array(10).fill("");
 
-    const calculateGrades = () => {
-        console.log(year, branch);
-        console.log(data[year][branch]);
-        if (year === 1 && branch === 'cyber') {
-            subject1 = data[year][branch][0];
-            subject2 = data[year][branch][1];
-        } else if (year === 1 && branch === 'aids') {
-            subject1 = data[year][branch][0];
-            subject2 = data[year][branch][1];
-        } else if (year === 1 && branch === 'iot') {
-            subject1 = data[year][branch][0];
-            subject2 = data[year][branch][1];
-        } else {
-            subject1 = '';
-            subject2 = '';
+    function calculateHandler() {
+        console.log('Calculating....');
+
+        let totalCredits = 0;
+        let weightedSum = 0;
+
+        for (let idx = 0; idx < Object.keys(data[sem][branch]).length; idx++) {
+            const selectedGrade = parseInt(grade[idx]);
+            const [courseCode, credit] = Object.entries(data[sem][branch])[idx];
+
+            if (!isNaN(selectedGrade)) {
+                totalCredits += credit as number;
+                weightedSum += selectedGrade * (credit as number);
+            }
         }
-    };
 
-    // onMount(() => {
-    //     calculateGrades();
-    // });
+        const finalGPA = totalCredits !== 0 ? weightedSum / totalCredits : 0;
 
+        console.log('Total Credits:', totalCredits);
+        console.log('Weighted Sum:', weightedSum);
+        console.log('Final GPA:', finalGPA);
+
+        saveDataToLocalStorage();
+    }
+
+    function clearItems() {
+        for (let i = 0; i < 10; i++) {
+            grade[i] = "";
+        }
+    }
+
+    function saveDataToLocalStorage() {
+        localStorage.setItem('grades', JSON.stringify(grade));
+        localStorage.setItem('sem', sem.toString());
+        localStorage.setItem('branch', branch);
+    }
+
+    function retrieveDataFromLocalStorage() {
+        const storedGrades = localStorage.getItem('grades');
+        const storedSem = localStorage.getItem('sem');
+        const storedBranch = localStorage.getItem('branch');
+        if (storedGrades && storedSem && storedBranch) {
+            grade = JSON.parse(storedGrades);
+            sem = storedSem;
+            branch = storedBranch;
+        }
+    }
+
+    onMount(() => {
+        retrieveDataFromLocalStorage();
+    });
 </script>
 
-<h1>Calculator</h1>
+<h1 class="flex justify-center text-blue-500 text-5xl">Calculator</h1>
 
-<p>Calculate your grade here!</p>
+<p class="flex justify-center text-blue-400 text-3xl">Calculate your grade here!</p>
 
-<div>
-    <label for="year">Year:</label>
-    <select id="year" bind:value={year}>
-        <option value="" selected disabled>Select Year</option>
+<br>
+<br>
+<div class="flex pl-40">
+    <!-- <label for="sem" class="text-blue-700 text-3xl">Semester</label> -->
+    <select id="sem" class="text-blue-400 text-2xl bg-inherit" bind:value={sem} on:change={clearItems}>
+        <option value="" class="" selected disabled>Sem</option>
         <!-- Default value -->
-        <option value="1">1</option>
-        <option value="2">2</option>
-        <option value="3">3</option>
+        <option value="1">1st Semester</option>
+        <option value="2">2nd Semester</option>
+        <option value="3">3rd Semester</option>
+        <option value="4">4th Semester</option>
+        <option value="5">5th Semester</option>
+        <option value="6">6th Semester</option>
+        <!-- <option value="7" disabled>7</option>
+        <option value="8" disabled>8</option> -->
     </select>
 </div>
 
-<div>
-    <label for="branch">Branch:</label>
-    <select id="branch" bind:value={branch}>
-        <option value="" selected disabled>Select Branch</option>
+<div class="flex pl-40">
+    <!-- <label for="branch" class="">Branch:</label> -->
+    <select id="branch" class="text-blue-400 text-2xl bg-inherit" bind:value={branch} on:change={clearItems}>
+        <option value="" selected disabled>Branch</option>
         <!-- Default value -->
         <option value="aids">AIDS</option>
         <option value="iot">IoT</option>
         <option value="cyber">Cyber</option>
     </select>
 </div>
+    
+{#if branch && sem}
+<table class="rounded-xl border border-black">
+    <thead>
+      <tr>
+        <th class="border border-black p-2">Course Code</th>
+        <th class="border border-black p-2">Credit</th>
+        <th class="border border-black p-2">Grade</th>
+      </tr>
+    </thead>
+    <tbody>
+      {#each Object.entries(data[sem][branch]) as [courseCode, credit], idx}
+        <tr>
+          <td class="border border-black">{courseCode}</td>
+          <td class="border border-black text-center">{credit}</td>
+          <td class="border border-black">
+            <select id="sem" bind:value={grade[idx]} on:change={() => { console.log(grade)} }>
+              <option value="" selected disabled>Select Grade</option>
+              <option value=10>O</option>
+              <option value=9>A+</option>
+              <option value=8>A</option>
+              <option value=7>B+</option>
+              <option value=6>B</option>
+              <option value=5>P</option>
+              <option value=0>RE/AB</option>
+            </select>
+          </td>
+        </tr>
+      {/each}
+    </tbody>
+  </table>
 
-<button on:click={calculateGrades}>Next</button>
-
-{#if subject1 || subject2}
-    <div>
-        <label for={subject1}>{subject1}</label>
-        <input type="text" id={subject1}>
-    </div>
-
-    <div>
-        <label for={subject2}>{subject2}</label>
-        <input type="text" id={subject2}>
-    </div>
+  <button on:click={calculateHandler}>Calculate</button>
 {/if}
