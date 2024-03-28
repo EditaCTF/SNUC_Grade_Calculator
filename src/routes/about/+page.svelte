9f22f882e19f71a679b6c9804e190c482f45683b
try3d <rsarans186@gmail.com>
2024-03-28 14:37:11 +0530

    Pix pading isu

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,98 +1,105 @@
 <script lang="ts">
 </script>
 
-<div class='flex justify-left items-center h-screen px-10'>
-    <div class='text-left'>
-        <h1 class='py-50 text-4xl'>Made by</h1><br />
-        <div>
-            <div class='flex flex-col'>
-                <div class='flex'>
-                    <div class='flex'>
-                        <div class='flex bg-[#DAE9FF] rounded-xl w-192 px-5 py-5'>
-                            <div class='flex items-center'>
-                                <div class='flex'>
-                                    <img src='/hotaru.png' alt='hotaru' />
-                                </div>
-                            </div>
-                            <div class='flex items-center ml-2'>
-                                <div>
-                                    <p class='px-10 py-5 text-xl'>hotaru</p>
-                                </div>
-                                <div>
-                                    <p class='text-xs color-grey'>Prajesh Raam</p>
-                                </div> 
-                            </div>
-                            <div class='flex items-center ml-5'>
-                                <img class='px-2' src='discord.png' alt='discord' />
-                                <img class='px-2' src='github.png' alt='github' />
-                                <img class='px-2' src='linkedin.png' alt='linkedin' />
-                                <img class='px-2' src='x.png' alt='x' />
-                            </div>
-                        </div>
-                        <div class='flex bg-[#DAE9FF] rounded-xl w-192 px-5 py-5 ml-5'>
-                            <div class='flex items-center'>
-                                <div class='flex'>
-                                    <img src='/frenzy.png' alt='vjn' />
-                                </div>
-                            </div>
-                            <div class='flex items-center ml-2'>
-                                <div>
-                                    <p class='px-10 py-5 text-xl'>FrenzyVJN</p>
-                                </div>
-                                <div>
-                                    <p class='text-xs color-grey'>Vijayan Sankar</p>
-                                </div> 
-                            </div>
-                            <div class='flex items-center ml-5'>
-                                <img class='px-2' src='discord.png' alt='discord' />
-                                <img class='px-2' src='github.png' alt='github' />
-                                <img class='px-2' src='linkedin.png' alt='linkedin' />
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class='flex mt-5'>
-                    <div class='flex'>
-                        <div class='flex bg-[#DAE9FF] rounded-xl w-192 px-5 py-5'>
-                            <div class='flex items-center'>
-                                <div class='flex'>
-                                    <img src='/try3d.png' alt='try3d' />
-                                </div>
-                            </div>
-                            <div class='flex items-center ml-2'>
-                                <div>
-                                    <p class='px-10 py-5 text-xl'>Try3D</p>
-                                </div>
-                                <div>
-                                    <p class='text-xs color-grey'>Saran Shankar</p>
-                                </div> 
-                            </div>
-                            <div class='flex items-center ml-5'>
-                                <img class='px-2' src='discord.png' alt='discord' />
-                                <img class='px-2' src='github.png' alt='github' />
-                                <img class='px-2' src='linkedin.png' alt='linkedin' />
-                            </div>
-                        </div>
-                        <div class='flex bg-[#DAE9FF] rounded-xl w-192 px-5 py-5 ml-5'>
-                            <div class='flex items-center'>
-                                <div class='flex'>
-                                    <img src='/edita.png' alt='edita' />
-                                </div>
-                            </div>
-                            <div class='flex items-center ml-2'>
-                                <div>
-                                    <p class='px-10 py-5 text-xl'>Team Edita</p>
-                                </div>
-                            </div>
-                            <div class='flex items-center ml-5'>
-                                <img class='px-2' src='discord.png' alt='discord' />
-                                <img class='px-2' src='github.png' alt='github' />
-                                <img class='px-2' src='x.png' alt='x' />
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+<div class="flex justify-left items-center h-screen px-10">
+	<div class="text-left">
+		<h1 class="py-50 text-4xl">Made by</h1>
+		<br />
+		<div>
+			<div class="flex flex-col">
+				<div class="flex">
+					<div class="flex">
+						<div class="flex bg-[#DAE9FF] rounded-xl w-192 px-5 py-5">
+							<div class="flex items-center">
+								<div class="flex">
+									<img src="/hotaru.png" alt="hotaru" />
+								</div>
+							</div>
+							<div class="flex items-center ml-2">
+								<div>
+									<p class="px-10 py-5 text-xl">hotaru</p>
+								</div>
+								<div>
+									<p class="text-xs color-grey">Prajesh Raam</p>
+								</div>
+							</div>
+							<div class="flex items-center ml-5">
+								<img class="px-2" src="discord.png" alt="discord" />
+								<img class="px-2" src="github.png" alt="github" />
+								<img class="px-2" src="linkedin.png" alt="linkedin" />
+								<img class="px-2" src="x.png" alt="x" />
+							</div>
+						</div>
+						<div class="flex bg-[#DAE9FF] rounded-xl w-192 px-5 py-5 ml-5">
+							<div class="flex items-center">
+								<div class="flex">
+									<img src="/frenzy.png" alt="vjn" />
+								</div>
+							</div>
+							<div class="flex items-center ml-2">
+								<div>
+									<p class="px-10 py-5 text-xl">FrenzyVJN</p>
+								</div>
+								<div>
+									<p class="text-xs color-grey">Vijayan Sankar</p>
+								</div>
+							</div>
+							<div class="flex items-center ml-5">
+								<img class="px-2" src="discord.png" alt="discord" />
+								<img class="px-2" src="github.png" alt="github" />
+								<img class="px-2" src="linkedin.png" alt="linkedin" />
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="flex mt-5">
+					<div class="flex">
+						<div class="flex bg-[#DAE9FF] rounded-xl w-192 px-5 py-5">
+							<div class="flex items-center">
+								<div class="flex">
+									<img src="/try3d.png" alt="try3d" />
+								</div>
+							</div>
+							<div class="flex items-center ml-2">
+								<div>
+									<p class="px-10 py-5 text-xl">Try3D</p>
+								</div>
+								<div>
+									<p class="text-xs color-grey">Saran Shankar</p>
+								</div>
+							</div>
+							<div class="flex items-center ml-5">
+								<img class="px-2" src="discord.png" alt="discord" />
+								<img class="px-2" src="github.png" alt="github" />
+								<img class="px-2" src="linkedin.png" alt="linkedin" />
+							</div>
+						</div>
+						<div class="flex bg-[#DAE9FF] rounded-xl w-192 px-5 py-5 ml-5">
+							<div class="flex items-center">
+								<div class="flex">
+									<img src="/edita.png" alt="edita" />
+								</div>
+							</div>
+							<div class="flex items-center ml-2">
+								<div>
+									<p class="px-10 py-5 text-xl">Team Edita</p>
+								</div>
+							</div>
+							<div class="flex items-center ml-5">
+								<img class="px-2" src="discord.png" alt="discord" />
+								<img class="px-2" src="github.png" alt="github" />
+								<img class="px-2" src="x.png" alt="x" />
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
+
+<style>
+	.w-192 {
+		width: 600px;
+	}
+</style>
