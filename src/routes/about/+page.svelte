f0a2134e7d53e72cbbfd15c2b39de97318b6f65f
hotaru <hotaru.py@gmail.com>
2024-03-28 13:40:03 +0530

    minor ui changes

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,6 +1,25 @@
 <script lang="ts">
 </script>
 
-<main class="bg-inherit min-h-screen">
-<div>This amazing page is brought to you by Edita</div>
-</main>
+<div class='flex justify-left items-center h-screen px-10'>
+    <div class='text-left'>
+        <h1 class='py-50 text-4xl'>Made by</h1><br />
+        <div>
+            <div>
+                <div class='flex bg-[#DAE9FF] rounded-xl w-fit px-5 py-5'>
+                    <div class='flex-col'>
+                        <img src='/hotaru.png' alt='hotaru' />
+                    </div>
+                    <div class='flex'>
+                        <div>
+                            <p class='px-10 py-5 text-xl'>hotaru</p>
+                        </div>
+                        <div>
+                            <p class='text-xs color-grey'>Prajesh Raam</p>
+                        </div>    
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
