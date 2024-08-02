6fb59cf33ddd6fba2b3013a5aa16411ad5f5d2cf
hotaru <hotaru.py@gmail.com>
2024-08-02 17:29:21 +0530

    more minor changes

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,62 +1,47 @@
 <script lang="ts">
 </script>
 
-<div class="flex flex-wrap justify-center items-center min-h-screen px-10">
+<div
+	class="flex flex-wrap justify-center items-center min-h-screen px-10"
+	style="padding-bottom: 10%;"
+>
 	<div class="text-left w-full">
-		<h1 class="py-5 text-4xl text-center">Made by</h1>
+		<h1 class="text-4xl text-center">Made by</h1>
 		<br />
 		<div class="grid grid-cols-1 md:grid-cols-2 gap-4 mt-5">
 			<!-- hotaru card -->
 			<div class="flex flex-col items-center bg-[#DAE9FF] rounded-xl w-full px-5 py-5 shadow-lg">
-				<img src="/hotaru.png" alt="hotaru" />
+				<img class="h-32" src="/hotaru.webp" alt="hotaru" />
 				<p class="text-xl mt-5">hotaru</p>
 				<div class="flex space-x-2 mt-3">
 					<img
 						class="hover:cursor-pointer h-4"
-						src="discord.png"
-						alt="discord"
-						onclick="window.open('https://discord.com/users/528963356082700289')"
-					/>
-					<img
-						class="hover:cursor-pointer h-4"
-						src="github.png"
+						src="github.webp"
 						alt="github"
 						onclick="window.open('https://github.com/hotaru-hspr')"
 					/>
 					<img
 						class="hover:cursor-pointer h-4"
-						src="linkedin.png"
+						src="linkedin.webp"
 						alt="linkedin"
 						onclick="window.open('https://www.linkedin.com/in/prajesh-raam/')"
-					/>
-					<img
-						class="hover:cursor-pointer h-4"
-						src="x.png"
-						alt="x"
-						onclick="window.open('https://x.com/hotaru_hspr')"
 					/>
 				</div>
 			</div>
 			<!-- FrenzyVJN card -->
 			<div class="flex flex-col items-center bg-[#DAE9FF] rounded-xl w-full px-5 py-5 shadow-lg">
-				<img src="/frenzy.png" alt="vjn" />
+				<img class="h-32" src="/frenzy.webp" alt="vjn" />
 				<p class="text-xl mt-5">FrenzyVJN</p>
 				<div class="flex space-x-2 mt-3">
 					<img
 						class="hover:cursor-pointer h-4"
-						src="discord.png"
-						alt="discord"
-						onclick="window.open('https://discord.com/users/813371659738021919')"
-					/>
-					<img
-						class="hover:cursor-pointer h-4"
-						src="github.png"
+						src="github.webp"
 						alt="github"
 						onclick="window.open('https://github.com/FrenzyVJN')"
 					/>
 					<img
 						class="hover:cursor-pointer h-4"
-						src="linkedin.png"
+						src="linkedin.webp"
 						alt="linkedin"
 						onclick="window.open('https://www.linkedin.com/in/vijayan-sankar/')"
 					/>
@@ -64,30 +49,24 @@
 			</div>
 			<!-- Try3D card -->
 			<div class="flex flex-col items-center bg-[#DAE9FF] rounded-xl w-full px-5 py-5 shadow-lg">
-				<img src="/try3d.png" alt="try3d" />
+				<img class="h-32" src="/try3d.webp" alt="try3d" />
 				<p class="text-xl mt-5">Try3D</p>
 				<div class="flex space-x-2 mt-3">
 					<img
 						class="hover:cursor-pointer h-4"
-						src="discord.png"
-						alt="discord"
-						onclick="window.open('https://discord.com/users/670216971207573504')"
-					/>
-					<img
-						class="hover:cursor-pointer h-4"
-						src="github.png"
+						src="github.webp"
 						alt="github"
 						onclick="window.open('https://github.com/Try3D')"
 					/>
 					<img
 						class="hover:cursor-pointer h-4"
-						src="linkedin.png"
+						src="linkedin.webp"
 						alt="linkedin"
 						onclick="window.open('https://www.linkedin.com/in/saranshankar/')"
 					/>
 					<img
 						class="hover:cursor-pointer h-4"
-						src="x.png"
+						src="x.webp"
 						alt="x"
 						onclick="window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ')"
 					/>
@@ -95,24 +74,24 @@
 			</div>
 			<!-- Team Edita card -->
 			<div class="flex flex-col items-center bg-[#DAE9FF] rounded-xl w-full px-5 py-5 shadow-lg">
-				<img src="/edita.png" alt="edita" />
+				<img class="h-32" src="/edita.webp" alt="edita" />
 				<p class="text-xl mt-5">Team Edita</p>
 				<div class="flex space-x-2 mt-3">
 					<img
 						class="hover:cursor-pointer h-4"
-						src="discord.png"
+						src="discord.webp"
 						alt="discord"
 						onclick="window.open('https://discord.gg/Qr3PsJfRF4')"
 					/>
 					<img
 						class="hover:cursor-pointer h-4"
-						src="github.png"
+						src="github.webp"
 						alt="github"
 						onclick="window.open('https://github.com/EditaCTF')"
 					/>
 					<img
 						class="hover:cursor-pointer h-4"
-						src="x.png"
+						src="x.webp"
 						alt="x"
 						onclick="window.open('https://x.com/editactf')"
 					/>
@@ -121,9 +100,3 @@
 		</div>
 	</div>
 </div>
-
-<style>
-	.w-192 {
-		width: 1500px;
-	}
-</style>
