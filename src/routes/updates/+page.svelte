98db897b1ebcb2cdc699af1f59c4aa165ae47e5f
hotaru <hotaru.py@gmail.com>
2024-08-02 15:03:17 +0530

    bcom fix

--- src/routes/updates/+page.svelte
+++ src/routes/updates/+page.svelte
@@ -2,8 +2,8 @@
 	<div class="text-left">
 		<h1 class="py-50 text-4xl">Latest update</h1>
 		<br />
-		<p class="text-2x1">v24.08.02</p>
-		<li>B.Com and B.Sc has been added to the calculator.</li>
+		<p class="text-2x1">v24.08.02 fix 1</p>
+		<li>B.Com and B.Com PA courses fixed. We apologize for the error!</li>
 		<!-- <li>Some errors in credit count has been fixed in Semester 3 and 4 of B.Tech.</li> -->
 		<p></p>
 		<br />
