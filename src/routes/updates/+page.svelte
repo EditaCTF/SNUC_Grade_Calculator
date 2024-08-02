56b30ff1e2257fe3f883d8d510a4a0cee895a931
hotaru <hotaru.py@gmail.com>
2024-08-02 15:21:38 +0530

    bsc fix 2

--- src/routes/updates/+page.svelte
+++ src/routes/updates/+page.svelte
@@ -3,7 +3,10 @@
 		<h1 class="py-50 text-4xl">Latest update</h1>
 		<br />
 		<p class="text-2x1">v24.08.02 fix 2</p>
-		<li>B.Com, B.Com PA and B.Sc subjects and credits fixed. We apologize for the error!</li>
+		<li>
+			B.Com, B.Com PA and B.Sc subjects and credits fixed for Semester 2. We apologize for the
+			error!
+		</li>
 		<!-- <li>Some errors in credit count has been fixed in Semester 3 and 4 of B.Tech.</li> -->
 		<p></p>
 		<br />
