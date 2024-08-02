6d38dff7d0f2cd848d6a5a1b0f5d5b67805ed9eb
hotaru <hotaru.py@gmail.com>
2024-08-02 15:41:46 +0530

    colour changes + navbar logo fix

--- src/routes/Navbar.svelte
+++ src/routes/Navbar.svelte
@@ -14,7 +14,7 @@
 <nav class="flex justify-between items-center py-2 px-6">
 	<div class="">
 		<a href="/">
-			<img src="/image.png" alt="Logo" class="md:h-16 md:mr-2" />
+			<img src="/image.png" alt="Logo" class="h-12 md:h-16 lg:h-20" />
 		</a>
 	</div>
 
