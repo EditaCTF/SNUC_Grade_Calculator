42979e2d0f2083c6cefcdb8882f340d21a24eeee
Try3D <rsarans186@gmail.com>
2024-08-04 10:21:04 +0530

    Semantic HTML tags instead of onclicks

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -14,18 +14,12 @@
 				<img class="h-32" src="/hotaru.webp" alt="hotaru" />
 				<p class="text-xl mt-5">hotaru</p>
 				<div class="flex space-x-2 mt-3">
-					<img
-						class="hover:cursor-pointer h-4"
-						src="github.webp"
-						alt="github"
-						onclick="window.open('https://github.com/hotaru-hspr')"
-					/>
-					<img
-						class="hover:cursor-pointer h-4"
-						src="linkedin.webp"
-						alt="linkedin"
-						onclick="window.open('https://www.linkedin.com/in/prajesh-raam/')"
-					/>
+					<a href="https://github.com/hotaru-hspr" target="_blank">
+						<img class="hover:cursor-pointer h-4" src="github.webp" alt="github" />
+					</a>
+					<a href="https://www.linkedin.com/in/prajesh-raam/" target="_blank">
+						<img class="hover:cursor-pointer h-4" src="linkedin.webp" alt="linkedin" />
+					</a>
 				</div>
 			</div>
 			<!-- FrenzyVJN card -->
@@ -33,18 +27,12 @@
 				<img class="h-32" src="/frenzy.webp" alt="vjn" />
 				<p class="text-xl mt-5">FrenzyVJN</p>
 				<div class="flex space-x-2 mt-3">
-					<img
-						class="hover:cursor-pointer h-4"
-						src="github.webp"
-						alt="github"
-						onclick="window.open('https://github.com/FrenzyVJN')"
-					/>
-					<img
-						class="hover:cursor-pointer h-4"
-						src="linkedin.webp"
-						alt="linkedin"
-						onclick="window.open('https://www.linkedin.com/in/vijayan-sankar/')"
-					/>
+					<a href="https://github.com/FrenzyVJN" target="_blank">
+						<img class="hover:cursor-pointer h-4" src="github.webp" alt="github" />
+					</a>
+					<a href="https://www.linkedin.com/in/vijayan-sankar/" target="_blank">
+						<img class="hover:cursor-pointer h-4" src="linkedin.webp" alt="linkedin" />
+					</a>
 				</div>
 			</div>
 			<!-- Try3D card -->
@@ -52,24 +40,15 @@
 				<img class="h-32" src="/try3d.webp" alt="try3d" />
 				<p class="text-xl mt-5">Try3D</p>
 				<div class="flex space-x-2 mt-3">
-					<img
-						class="hover:cursor-pointer h-4"
-						src="github.webp"
-						alt="github"
-						onclick="window.open('https://github.com/Try3D')"
-					/>
-					<img
-						class="hover:cursor-pointer h-4"
-						src="linkedin.webp"
-						alt="linkedin"
-						onclick="window.open('https://www.linkedin.com/in/saranshankar/')"
-					/>
-					<img
-						class="hover:cursor-pointer h-4"
-						src="x.webp"
-						alt="x"
-						onclick="window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ')"
-					/>
+					<a href="https://github.com/Try3D" target="_blank">
+						<img class="hover:cursor-pointer h-4" src="github.webp" alt="github" />
+					</a>
+					<a href="https://www.linkedin.com/in/saranshankar/" target="_blank">
+						<img class="hover:cursor-pointer h-4" src="linkedin.webp" alt="linkedin" /></a
+					>
+					<a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">
+						<img class="hover:cursor-pointer h-4" src="x.webp" alt="x" />
+					</a>
 				</div>
 			</div>
 			<!-- Team Edita card -->
@@ -77,24 +56,15 @@
 				<img class="h-32" src="/edita.webp" alt="edita" />
 				<p class="text-xl mt-5">Team Edita</p>
 				<div class="flex space-x-2 mt-3">
-					<img
-						class="hover:cursor-pointer h-4"
-						src="discord.webp"
-						alt="discord"
-						onclick="window.open('https://discord.gg/Qr3PsJfRF4')"
-					/>
-					<img
-						class="hover:cursor-pointer h-4"
-						src="github.webp"
-						alt="github"
-						onclick="window.open('https://github.com/EditaCTF')"
-					/>
-					<img
-						class="hover:cursor-pointer h-4"
-						src="x.webp"
-						alt="x"
-						onclick="window.open('https://x.com/editactf')"
-					/>
+					<a href="https://discord.gg/Qr3PsJfRF4" target="_blank">
+						<img class="hover:cursor-pointer h-4" src="discord.webp" alt="discord" />
+					</a>
+					<a href="https://github.com/EditaCTF" target="_blank">
+						<img class="hover:cursor-pointer h-4" src="github.webp" alt="github" />
+					</a>
+					<a href="https://x.com/editactf" target="_blank">
+						<img class="hover:cursor-pointer h-4" src="x.webp" alt="x" />
+					</a>
 				</div>
 			</div>
 		</div>
